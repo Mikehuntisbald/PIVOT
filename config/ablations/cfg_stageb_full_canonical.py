4d2f922e803bd94254ceb346f00bed115663d8ec
Haoyi <db72702@um.edu.mo>
2026-05-19 15:39:46 +0800

    Clean ablation config formatting

--- config/ablations/cfg_stageb_full_canonical.py
+++ config/ablations/cfg_stageb_full_canonical.py
@@ -2,4 +2,3 @@ from config.cfg_patch_stage_b import *  # noqa: F401,F403
 
 canonical_pos_weight = 1.0
 stage_b_infer_canonical_weight = 1.0
-
--- config/ablations/cfg_stageb_no_tn.py
+++ config/ablations/cfg_stageb_no_tn.py
@@ -1,2 +1,1 @@
 from config.cfg_patch_stage_b import *  # noqa: F401,F403
-
